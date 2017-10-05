cb1f65de1b674035013ee20a714622095e4fb777
DDAACC <2248080961@qq.com>
2017-10-05 16:41:17 +0800

    up

--- towerDefance/src/app/data/data_monster.lua
+++ towerDefance/src/app/data/data_monster.lua
@@ -2,7 +2,7 @@ local data_monster={}
 
 data_monster[1]={
 	name="1级怪",
-	hp=600,
+	hp=50,
 	speed=50,
 	dmg=5,
 	pic={"monster01-up.png","monster01-down.png","monster01-left.png","monster01-right.png"},
@@ -12,35 +12,55 @@ data_monster[1]={
 
 data_monster[2]={
 	name="2级怪",
-	hp=100,
+	hp=200,
 	speed=80,
 	dmg=10,
-	pic="monster02.png"
+	pic={"monster01-up.png","monster01-down.png","monster01-left.png","monster01-right.png"},
+	introduction="描述:绿毛虫,拥有较低的生命值和移动速度",
+	skill="技能:无"
 }
 
 data_monster[3]={
 	name="3级怪",
-	hp=150,
+	hp=700,
 	speed=110,
 	dmg=15,
-	pic="monster02.png"
+	pic={"monster01-up.png","monster01-down.png","monster01-left.png","monster01-right.png"},
+	introduction="描述:绿毛虫,拥有较低的生命值和移动速度",
+	skill="技能:无"
 }
 
 data_monster[4]={
 	name="4级怪",
-	hp=200,
+	hp=1200,
 	speed=140,
 	dmg=20,
-	pic="monster02.png"
+	pic={"monster01-up.png","monster01-down.png","monster01-left.png","monster01-right.png"},
+	introduction="描述:绿毛虫,拥有较低的生命值和移动速度",
+	skill="技能:无"
 }
 
 data_monster[5]={
 	name="5级怪",
-	hp=250,
+	hp=1500,
 	speed=170,
 	dmg=25,
-	pic="monster02.png"
+	pic={"monster01-up.png","monster01-down.png","monster01-left.png","monster01-right.png"},
+	introduction="描述:绿毛虫,拥有较低的生命值和移动速度",
+	skill="技能:无"
 }
+
+
+data_monster[6]={
+	name="Boss",
+	hp=50000,
+	speed=30,
+	dmg=99,
+	pic={"monster01-up.png","monster01-down.png","monster01-left.png","monster01-right.png"},
+	introduction="描述:1级最终boss",
+	skill="技能:无"
+}
+
 
 
 return data_monster
